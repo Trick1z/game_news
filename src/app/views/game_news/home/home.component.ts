ad5e025802c2ac6afa8a054d0c14299769860078
trick1z <tiker.zone.teen@gmail.com>
2025-03-11 00:01:18 +0700

    s

--- src/app/views/game_news/home/home.component.ts
+++ src/app/views/game_news/home/home.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ApiServices } from '../apiServices';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  ngOnInit(): void {
+    this.get_data()
+  }
+
+  constructor(
+    private http :HttpClient,
+    private Base :ApiServices
+  ){}
+
+  get_data(){
+    this.http.get(`${this.Base.Api()}/get.user.main`).subscribe((res:any) =>{
+      console.log(res);
+
+    })
+  }
 }
