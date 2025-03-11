61b101d80d5d7122248acb7ca8afd5795998e504
trick1z <tiker.zone.teen@gmail.com>
2025-03-11 13:59:08 +0700

    add card

--- src/app/views/game_news/home/home.component.ts
+++ src/app/views/game_news/home/home.component.ts
@@ -1,27 +1,24 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ApiServices } from '../apiServices';
+import { CommonModule, NgFor } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [HttpClientModule],
+  imports: [HttpClientModule,CommonModule,NgFor],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent implements OnInit{
-
+export class HomeComponent implements OnInit {
   ngOnInit(): void {
-    this.get_data()
+    this.get_data();
   }
 
-  constructor(
-    private http :HttpClient,
-    private Base :ApiServices
-  ){}
-
-  get_data(){
-    this.http.get(`${this.Base.Api()}/get.user.main`).subscribe((res:any) =>{
+  constructor(private http: HttpClient, private Base: ApiServices) {}
+  _data: any = [];
+  get_data() {
+    this.http.get(`${this.Base.Api()}/get.user.main`).subscribe((res: any) => {
       console.log(res);
-
-    })
+      this._data = res;
+    });
   }
 }
